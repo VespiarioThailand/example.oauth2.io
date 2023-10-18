0c04797e062b54825bce86577d539c8235b8be64
pramot-dept <pramot@dept.email>
2023-10-18 13:54:15 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  output: 'export',
+ 
   reactStrictMode: true,
 
  // Add basePath
   basePath: '/github-pages',
-
 }
-
+ 
 module.exports = nextConfig
