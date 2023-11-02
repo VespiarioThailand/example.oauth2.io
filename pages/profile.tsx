ea68c4aa516c5c153306636b069a0a8ba83634c0
pramot-dept <pramot@dept.email>
2023-11-02 11:32:26 +0700

    fix dark mode

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -68,49 +68,51 @@ const Profile = () => {
 
   return profile ? (
     <div className="grid place-items-center">
-      <h2>Auth</h2>
-      <div className="w-96 text-ellipsis ">
-        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Access Token
-        </label>
-        <textarea
-          id="message"
-          rows={4}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          value={token.access_token}
-        ></textarea>
+      <div className="bg-slate-100 mt-8 p-8 dark:bg-slate-800 grid place-items-center">
+        <h2>Auth</h2>
+        <div className="w-96 text-ellipsis ">
+          <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Access Token
+          </label>
+          <textarea
+            id="message"
+            rows={4}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={token.access_token}
+          ></textarea>
 
-        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Refresh Token
-        </label>
-        <textarea
-          id="message"
-          rows={1}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          value={token.refresh_token}
-        ></textarea>
-        {/* <p>Refresh Token: {token.refresh_token}</p> */}
-      </div>
-      <br />
-      <button
-        type="button"
-        className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
-        onClick={handleSignOut}
-      >
-        Sign out
-      </button>
-      <br />
-      <h2>Profile</h2>
-      <div>
-        {profile ? (
-          <>
-            <p>Full name: {profile?.full_name}</p>
-            <p>Email: {profile.email || '-'}</p>
-            <p>Mobile: {profile.phone_number || '-'}</p>
-            <p>Vespisti ID : {profile.vespisti_code}</p>
-            <p>Profile image: {profile.profile_image ? <img src={profile.profile_image} alt="test" /> : null}</p>
-          </>
-        ) : null}
+          <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Refresh Token
+          </label>
+          <textarea
+            id="message"
+            rows={1}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={token.refresh_token}
+          ></textarea>
+          {/* <p>Refresh Token: {token.refresh_token}</p> */}
+        </div>
+        <br />
+        <button
+          type="button"
+          className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2"
+          onClick={handleSignOut}
+        >
+          Sign out
+        </button>
+        <br />
+        <h2>Profile</h2>
+        <div>
+          {profile ? (
+            <>
+              <p>Full name: {profile?.full_name}</p>
+              <p>Email: {profile.email || '-'}</p>
+              <p>Mobile: {profile.phone_number || '-'}</p>
+              <p>Vespisti ID : {profile.vespisti_code}</p>
+              <p>Profile image: {profile.profile_image ? <img src={profile.profile_image} alt="test" /> : null}</p>
+            </>
+          ) : null}
+        </div>
       </div>
     </div>
   ) : null
