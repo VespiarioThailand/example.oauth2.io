58e33bc6f0b821c4d1229dd34b78a80800933158
pramot <pramot@dept.email>
2023-11-21 12:36:00 +0700

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 import getConfig from 'next/config'
 
 export const getLocalStorage = (key: string, envName: string): string => {
-  const env = localStorage.getItem('app_en_2') ?? getConfig().publicRuntimeConfig['ENV']
-  return localStorage.getItem(key) ?? getConfig().publicRuntimeConfig[`${envName}_${env}`] ?? ''
+  const env = localStorage?.getItem('app_en_2') ?? getConfig().publicRuntimeConfig['ENV']
+  return localStorage?.getItem(key) ?? getConfig().publicRuntimeConfig[`${envName}_${env}`] ?? ''
 }
